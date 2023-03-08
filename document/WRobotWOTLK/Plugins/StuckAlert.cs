889ef2bfe037d080ef18deeb6c40738624841147
elseif2023 <127265295+elseif2023@users.noreply.github.com>
2023-03-08 12:42:12 +0800

    Add files via upload

---
+++ document/WRobotWOTLK/Plugins/StuckAlert.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Threading;
+using System.Windows.Forms;
+using robotManager.Helpful;
+using robotManager.Products;
+using wManager;
+using wManager.Plugin;
+using wManager.Wow.Helpers;
+using wResources;
+
+public class Main : IPlugin
+{
+    private bool _isLaunched;
+
+    public void Initialize()
+    {
+        _isLaunched = true;
+        var c = Statistics.Stucks;
+        SoundPlayer myPlayer;
+        if (File.Exists(Application.StartupPath + @"\Data\newWhisper.wav"))
+        {
+            myPlayer = new SoundPlayer
+            {
+                SoundLocation = Application.StartupPath + @"\Data\newWhisper.wav"
+            };
+        }
+        else
+        {
+            myPlayer = new SoundPlayer(Resource.NewWhisper);
+        }
+        Logging.Write("[StuckAlert] Started.");
+
+        while (_isLaunched && Products.IsStarted)
+        {
+            try
+            {
+                if (Conditions.InGameAndConnectedAndAliveAndProductStartedNotInPause)
+                {
+                    if (c != Statistics.Stucks)
+                    {
+                        myPlayer.Play();
+                        c = Statistics.Stucks;
+                    }
+                }
+                else
+                {
+                    c = Statistics.Stucks;
+                }
+            }
+            catch (Exception e)
+            {
+                Logging.WriteError("[StuckAlert]: " + e);
+            }
+            Thread.Sleep(150);
+        }
+    }
+
+    public void Dispose()
+    {
+        _isLaunched = false;
+        Logging.Write("[StuckAlert] Stoped.");
+    }
+
+    public void Settings()
+    {
+        Logging.Write("[StuckAlert] No setting.");
+    }
+}
---
+++ document/WRobotWOTLK/Plugins/Whisper Screenshot for private server.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+using robotManager.Helpful;
+using wManager.Wow;
+using wManager.Wow.Enums;
+using wManager.Wow.Helpers;
+
+public class Main : wManager.Plugin.IPlugin
+{
+    public void Initialize()
+    {
+        // For this plugin I use events, but you can create loop, sample: while (robotManager.Products.Products.IsStarted) {  Your Code Here }
+        // Attach function NewScreen() at lua event CHAT_MSG_WHISPER.
+        EventsLua.AttachEventLua(LuaEventsId.CHAT_MSG_WHISPER, m => NewScreen()); // LuaEventsId.CHAT_MSG_WHISPER
+        Logging.Write("[WhisperScreenshot] Loadded.");
+    }
+
+    public void Dispose()
+    {
+        Logging.Write("[WhisperScreenshot] Disposed.");
+    }
+
+    public void Settings()
+    {
+        MessageBox.Show("[WhisperScreenshot] No settings for this plugin.");
+    }
+
+    public static void NewScreen()
+    {
+        // Press key PrintScreen:
+        Keyboard.UpKey(Memory.WowMemory.Memory.WindowHandle, Keys.PrintScreen);
+        // Add log:
+        Logging.Write("[WhisperScreenshot] New Whisper! screenshot saved in World of ''Warcraft\\Screenshots'' folder.");
+    }
+}
